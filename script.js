31a67d95a40f4617329350c7fcdabea14f562e0d
Angus Macrae <angusdmacrae@gmail.com>
2020-05-15 17:18:57 +0800

    Added functionality for metric/imperial toggle

--- script.js
+++ script.js
@@ -15,12 +15,18 @@ let resCont = document.getElementById("display-box");
 let body = document.querySelector("body");
 let spinner = document.querySelector(".spinner");
 let alertBox = document.getElementById("alert-box");
+let miToggle = document.getElementById("mi-toggle");
 
-inputField.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    submitBtn.click();
-  }
+let units = 0;
+let temp = 0;
+let windSpeed = 0;
+let windDirection = 0;
+
+inputField.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        submitBtn.click();
+    }
 });
 
 submitBtn.addEventListener("click", function () {
@@ -28,7 +34,7 @@ submitBtn.addEventListener("click", function () {
     let city = inputField.value;
 
     if (city != "") {
-        
+
         resCont.classList.remove("visible");
         alertBox.classList.remove("visible");
         spinner.classList.add("visible");
@@ -40,38 +46,69 @@ submitBtn.addEventListener("click", function () {
 
 });
 
+miToggle.addEventListener("change", function () {
+
+    if (this.checked) {
+        units = 1;
+    } else {
+        units = 0;
+    }
+
+    updateResultsUnits();
+
+});
+
+function updateResultsUnits() {
+
+    if (units == 1) {
+
+        tempDisp.innerHTML = (Math.round(((temp * 9 / 5) + 32) * 10)) / 10 + " &deg;F";
+        windDisp.innerHTML = windDirection + " wind @ " + (Math.round((windSpeed * 2.237) * 10)) / 10 + " mph";        
+
+    } else {
+
+        tempDisp.innerHTML = (Math.round(temp * 10)) / 10 + " &deg;C";
+        windDisp.innerHTML = windDirection + " wind @ " + windSpeed + " m/s";
+
+    }
+
+}
+
 function fetchWeather(targetCity) {
 
     let url = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=' + targetCity + '&appid=e7034c9ccb454fc5547fec12cad8b5d4';
 
     fetch(url)
         .then(response => response.json())
-        .then(data => {displayWeather(data); console.log(data);})
+        .then(data => {
+            displayWeather(data);
+            console.log(data);
+        })
         .catch(error => {
             console.log("Fetch error ", error);
             alertBox.classList.add("visible");
             spinner.classList.remove("visible");
             submitBtn.classList.remove("collapsed");
-    });
+        });
 
 }
 
 function displayWeather(weather) {
-    
+
     let city = weather.name;
     let country = weather.sys.country;
     let main = weather.weather[0].main;
     let desc = weather.weather[0].description;
     let icon = weather.weather[0].icon;
     let clouds = weather.clouds.all;
-    let temp = weather.main.temp - 273.15;
-    let tempFeels = weather.main.temp.feels_like - 273.15;
+    temp = weather.main.temp - 273.15;
+    tempFeels = weather.main.temp.feels_like - 273.15;
     let humidity = weather.main.humidity;
-    let windSpeed = weather.wind.speed;
-    let windDirection = weather.wind.deg;
-    
+    windSpeed = weather.wind.speed;
+    windDirection = weather.wind.deg;
+
     let timezoneOffset = weather.timezone; // offset s from UTC
-    
+
     let userTime = new Date();
     let remoteTime = toRemoteTime(userTime, timezoneOffset);
     let formattedRemoteTime = unixToHumanTime(remoteTime);
@@ -81,12 +118,12 @@ function displayWeather(weather) {
 
     let sunsetTime = new Date((weather.sys.sunset + userTime.getTimezoneOffset() * 60 + timezoneOffset) * 1000);
     let formattedSunsetTime = formatHrsMins(sunsetTime);
-    
-//    let long = weather.coord.lon;
-//    let lat = weather.coord.lat;
-    
-    switch(true) {
-            
+
+    //    let long = weather.coord.lon;
+    //    let lat = weather.coord.lat;
+
+    switch (true) {
+
         case windDirection >= 22.5 || windDirection < 67.5:
             windDirection = "NE";
             break;
@@ -110,7 +147,7 @@ function displayWeather(weather) {
             break;
         default:
             windDirection = "N";
-        
+
     }
 
     let searchString = city + "%20" + main;
@@ -118,61 +155,65 @@ function displayWeather(weather) {
     let bgImg = new Image();
     bgImg.onload = showResults;
     bgImg.src = url;
-    
-    function showResults () {
+
+    function showResults() {
         title.classList.add("showing");
         spinner.classList.remove("visible");
         submitBtn.classList.remove("collapsed");
-        
+
         body.style.backgroundImage = "url(" + bgImg.src + ")";
         headingDisp.innerHTML = "Current weather in " + city + ", " + country;
         iconImg.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
         descDisp.innerHTML = desc.charAt(0).toUpperCase() + desc.substr(1);
         cloudsDisp.innerHTML = clouds + "% cloud cover";
-        tempDisp.innerHTML = (Math.round(temp*10))/10 + " &deg;C";
+        tempDisp.innerHTML = (Math.round(temp * 10)) / 10 + " &deg;C";
         humidityDisp.innerHTML = humidity + "% humidity";
-        windDisp.innerHTML = windDirection + " wind @ " + windSpeed + " m/s ";
+        windDisp.innerHTML = windDirection + " wind @ " + windSpeed + " m/s";
         currentTimeDisp.innerHTML = "The local time is " + formattedRemoteTime;
         sunriseTimeDisp.innerHTML = formattedSunriseTime;
         sunsetTimeDisp.innerHTML = formattedSunsetTime;
-        
+
+        if (units == 1) {
+            updateResultsUnits();
+        }
+
         resCont.classList.add("visible");
     }
 
 }
 
 function toRemoteTime(localTime, remoteOffset) {
-    
-    return localTime.getTime()/1000 + localTime.getTimezoneOffset() * 60 + remoteOffset;
-    
+
+    return localTime.getTime() / 1000 + localTime.getTimezoneOffset() * 60 + remoteOffset;
+
 }
 
 function formatHrsMins(inputDate) {
-    
+
     return pad(inputDate.getHours(), 2) + ":" + pad(inputDate.getMinutes(), 2);
-    
+
 }
 
 function unixToHumanTime(unix) {
-    
+
     let humanDate = new Date(unix * 1000);
     return formatHrsMins(humanDate);
-    
+
 }
 
 function pad(number, length) {
-   
+
     var str = '' + number;
     while (str.length < length) {
         str = '0' + str;
     }
-   
+
     return str;
 
 }
 
 function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function(txt){
+    return str.replace(/\w\S*/g, function (txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1);
     });
 }
